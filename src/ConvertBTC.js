5de937776927afba4dee5933a4fc000a91893916
Adams Alves <alves.adams@gmail.com>
2021-09-08 15:14:05 -0300

    Fix lint

--- src/ConvertBTC.js
+++ src/ConvertBTC.js
@@ -4,7 +4,7 @@ function convertBTC(currency = 'USD', amount = 1) {
   const url = `https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=${currency}&amount=${amount}`;
 
   request(url, (error, response, body) => {
-    let apiResponse = JSON.parse(body);
+    const apiResponse = JSON.parse(body);
     console.log(`${amount} BTC to ${currency} = ${apiResponse.price}`);
   });
 }
