9dda20ec5bd6a12b4c20f5d742c89bfac1ebef31
Adams Alves <alves.adams@gmail.com>
2021-09-08 12:46:57 -0300

    Add commander.js to create CLI

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,12 +1,13 @@
 const expect = require('chai').expect;
 const exec = require('child_process').exec;
 const btcConverter = './src/main.js';
+const pkg = require('../package.json');
 
 describe('Main CLI', () => {
   it('should return Hello World!', (done) => {
-    exec(btcConverter, (err, stdout, stderr) => {
+    exec(`${btcConverter} --version`, (err, stdout, stderr) => {
       if(err) throw err;
-      expect(stdout.replace('\n', '')).to.be.equal('Hello World!');
+      expect(stdout.replace('\n', '')).to.be.equal(pkg.version);
       done();
     });
   });
